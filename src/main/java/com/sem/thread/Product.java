820f44d281b6e9eacfd9ac302e32d58ee0211637
scorp <2528360654@QQ.com>
2016-12-18 11:53:22 +0800

    init

---
+++ src/main/java/com/sem/thread/Product.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.sem.thread;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**   
+ * @Title: ForkJoin1.java 
+ * @Package com.sem.thread 
+ * @Description: fork/join 
+ * @author xuy
+ * @date 2016年12月17日 下午11:05:58 
+ * @version V1.0   
+ */
+public class Product {
+	@Getter @Setter
+	private String name;
+	private double price;
+	
+}
